e55a857bad48be583f9883dbfd57dfc3774b9c25
torque ninja <sayyidkhan92@hotmail.com>
2021-07-20 15:36:08 +0800

    add security

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");  // Require mongoose library
 require("dotenv").config();   // Require the dotenv
+const restrictOrigin = require("./middlewares/restrictOrigin");
 
 const app = express();
 //database codes
@@ -18,7 +20,10 @@ mongoose
 
 //app codes
 const PORT = process.env.PORT || 5000;
+
+app.use(restrictOrigin); // for security
 app.use(express.json());
+app.use(morgan("dev")); // for API reporting
 
 //endpoints
 app.post("/ping", (req, res) => {
@@ -26,7 +31,6 @@ app.post("/ping", (req, res) => {
         status: "Server is up and running",
     });
 });
-
 
 app.listen(PORT, () => {
     console.log("Server started listening on port : ", PORT);
